14d72f0511cb8a0d2a42b0663a36a24ea0c23bcc
MCarbono <marcelo.carbono@tino.com.br>
2023-09-24 14:56:42 -0300

    refact: model to entity

--- entity/user.go
+++ entity/user.go
@@ -1,7 +1,21 @@
 package entity
 
+import "errors"
+
 type User struct {
 	ID       int
 	Username string
 	Balance  float64
+}
+
+func (u *User) Withdraw(amount float64) error {
+	if u.Balance < amount {
+		return errors.New("insufficient funds")
+	}
+	u.Balance -= amount
+	return nil
+}
+
+func (u *User) Deposit(amount float64) {
+	u.Balance += amount
 }
