73838915c5cc53691d8cb9f1d24d6c1974ef9d40
MCarbono <marcelo.carbono@tino.com.br>
2023-09-24 15:00:24 -0300

    refact: main

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"money-transfer-api/database"
+	"fmt"
+	"log"
+	"money-transfer-api/infra/controller"
+	"money-transfer-api/infra/database"
+	"money-transfer-api/infra/router"
+	"money-transfer-api/repository"
+	"money-transfer-api/service"
+	"money-transfer-api/uow"
+	"net/http"
 
 	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+const (
+	PORT = 3000
 )
 
 func main() {
@@ -12,4 +24,10 @@ func main() {
 		panic(err)
 	}
 	defer DB.Close()
+	uow := uow.NewUowImpl(DB)
+	userService := service.NewUser(DB, uow, repository.USER_REPOSITORY_POSTGRES)
+	controller := controller.NewUserController(*userService)
+	router := router.NewRouter(*controller)
+	fmt.Printf("Starting server on port %v\n", PORT)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", PORT), router))
 }
