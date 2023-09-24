0c20521abb77df678bb3f150cee0d3eb9885b300
MCarbono <marcelo.carbono@tino.com.br>
2023-09-24 19:55:05 -0300

    refact: database connection

--- infra/database/database.go
+++ infra/database/database.go
@@ -1,15 +1,29 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"time"
+)
 
-func Open() (DB *sql.DB, err error) {
-	DB, err = sql.Open("pgx", "host=localhost port=5432 user=money-api password=root dbname=money-api sslmode=disable")
+const maxRetries = 5
+const retryInterval = 2 * time.Second
+
+func Open(host string) (DB *sql.DB, err error) {
+	DB, err = sql.Open("pgx", fmt.Sprintf("host=%v port=5432 user=money-api password=root dbname=money-api sslmode=disable", host))
 	if err != nil {
 		return
 	}
-	err = DB.Ping()
-	if err != nil {
-		return
+	for i := 0; i < maxRetries; i++ {
+		err = DB.Ping()
+		if err == nil {
+			break
+		}
+		if err != nil {
+			fmt.Printf("Connection failed (Attempt %d): %v\n", i+1, err)
+			time.Sleep(retryInterval)
+		}
 	}
+	fmt.Println("Connected to the database!")
 	return
 }
