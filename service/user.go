f972d390226229d6bd41b10219b9acb177d19a09
MCarbono <marcelo.carbono@tino.com.br>
2023-09-25 00:20:36 -0300

    refact: userService transfer with retry

--- service/user.go
+++ service/user.go
@@ -8,47 +8,60 @@ import (
 )
 
 type User struct {
-	DB  *sql.DB
-	uow uow.Uow
+	DB                   *sql.DB
+	uow                  uow.Uow
+	transferTotalRetries int
 }
 
 func NewUser(db *sql.DB, uow uow.Uow) *User {
 	return &User{
-		DB:  db,
-		uow: uow,
+		DB:                   db,
+		uow:                  uow,
+		transferTotalRetries: 5,
 	}
 }
 
 func (u *User) Transfer(input *TransferInput) (err error) {
-	err = u.uow.Do(context.Background(), func(uow *uow.UowImpl) error {
-		ctx := context.Background()
-		repo, err := uow.GetUserRepository(ctx, "UserRepository")
-		if err != nil {
+	total := 0
+	for total < u.transferTotalRetries {
+		err = u.uow.Do(context.Background(), func(uow *uow.UowImpl) error {
+			ctx := context.Background()
+			repo, err := uow.GetUserRepository(ctx, "UserRepository")
+			if err != nil {
+				return err
+			}
+			debtorUser, err := repo.FindUserTx(ctx, input.DebtorID)
+			if err != nil {
+				return err
+			}
+			err = debtorUser.Withdraw(input.Amount)
+			if err != nil {
+				return err
+			}
+			err = repo.Withdraw(ctx, debtorUser)
+			if err != nil {
+				return err
+			}
+			beneficiaryUser, err := repo.FindUserTx(ctx, input.BeneficiaryID)
+			if err != nil {
+				return err
+			}
+			beneficiaryUser.Deposit(input.Amount)
+			err = repo.Deposit(ctx, beneficiaryUser)
 			return err
+		})
+		if err == nil {
+			break
 		}
-		debtorUser, err := repo.FindUserTx(ctx, input.DebtorID)
 		if err != nil {
-			return err
+			if err.Error() == "transaction already started" {
+				fmt.Println(err.Error())
+				total++
+				continue
+			}
+			return
 		}
-		err = debtorUser.Withdraw(input.Amount)
-		if err != nil {
-			return err
-		}
-		err = repo.Withdraw(ctx, debtorUser)
-		if err != nil {
-			return err
-		}
-		beneficiaryUser, err := repo.FindUserTx(ctx, input.BeneficiaryID)
-		if err != nil {
-			return err
-		}
-		beneficiaryUser.Deposit(input.Amount)
-		err = repo.Deposit(ctx, beneficiaryUser)
-		if err != nil {
-			return err
-		}
-		return nil
-	})
+	}
 	return
 }
 
